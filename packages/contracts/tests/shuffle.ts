e2fdffd2f7fd5fd31292a672d201f1bed53ac148
sam <geeksam820@gmail.com>
2023-04-06 10:41:50 +0800

    cleanup shuffle V1

--- packages/contracts/tests/shuffle.ts
+++ packages/contracts/tests/shuffle.ts
@@ -12,7 +12,6 @@ const buildBabyjub = require('circomlibjs').buildBabyjub;
 const snarkjs = require('snarkjs');
 
 const fs = require('fs');
-const path = require('path');
 const https = require('https')
  
 const HOME_DIR = require('os').homedir();
@@ -39,18 +38,6 @@ async function dnld_aws(file_name : string) {
 }
 
 const resourceBasePath = P0X_DIR;
-
-// Deploys contract for shuffle encrypt v1.
-async function deployShuffleEncrypt() {
-    const vk0 = await (await ethers.getContractFactory('ShuffleEncryptVerifierKey0')).deploy();
-    const vk1 = await (await ethers.getContractFactory('ShuffleEncryptVerifierKey1')).deploy();
-    return await (await ethers.getContractFactory('Shuffle_encryptVerifier', {
-        libraries: {
-            ShuffleEncryptVerifierKey0: vk0.address,
-            ShuffleEncryptVerifierKey1: vk1.address,
-        }
-    })).deploy();
-}
 
 // Depploys contract for decryption.
 async function deployDecrypt() {
@@ -92,128 +79,6 @@ describe('Shuffle test', function () {
             }
         ));
     });
-
-    it('Shuffle contract can function normally', async () => {
-        // Load metadata.
-        const shuffleEncryptWasmFile = resolve(resourceBasePath, './wasm/shuffle_encrypt.wasm');
-        const shuffleEncryptZkeyFile = resolve(resourceBasePath, './zkey/shuffle_encrypt.zkey');
-        const shuffleEncryptVkey = await snarkjs.zKey.exportVerificationKey(new Uint8Array(Buffer.from(readFileSync(shuffleEncryptZkeyFile))));
-
-        const decryptWasmFile = resolve(resourceBasePath, './wasm/decrypt.wasm');
-        const decryptZkeyFile = resolve(resourceBasePath, './zkey/decrypt.zkey');
-        const decryptVkey = await snarkjs.zKey.exportVerificationKey(new Uint8Array(Buffer.from(readFileSync(decryptZkeyFile))));
-
-        // Deploy Contracts
-        const shuffleEncryptVerifierContract = await deployShuffleEncrypt();
-        const decryptVerifierContract = await deployDecrypt();
-
-        const numCards = BigInt(52);
-        const numBits = BigInt(251);
-        const babyjub = await buildBabyjub();
-        // Generates secret/public key for each player. Each player should run this line.
-        // keys.pk: uint256 will be sent to smart contract.
-        // keys.sk: uint256 will be kept secret by each player.
-        const keysAlice = keyGen(babyjub, numBits);
-        const keysBob = keyGen(babyjub, numBits);
-        const keysCharlie = keyGen(babyjub, numBits);
-        // Compute aggregated key for all players. Each player should run this line on their own machine.
-        // No need to send this pk to smart contract.
-        const aggregatedPkEC = keyAggregate(babyjub, [keysAlice.pk, keysBob.pk, keysCharlie.pk]);
-        const aggregatePk = [babyjub.F.toString(aggregatedPkEC[0]), babyjub.F.toString(aggregatedPkEC[1])];
-        const skArray = [keysAlice.sk, keysBob.sk, keysCharlie.sk];
-        let pkArray = convertPk(babyjub, [keysAlice.pk, keysBob.pk, keysCharlie.pk]);
-        // Now shuffle the cards! Each player should run shuffleEncrypt.
-        // Output is the shuffled card Y and a proof.
-        // Alice's turn!
-        let A = samplePermutation(Number(numCards));
-        const X: bigint[] = initDeck(babyjub, Number(numCards));
-        let R = sampleFieldElements(babyjub, numBits, numCards);
-        let shuffleEncryptOutput = await generateShuffleEncryptProof(A, X, R, aggregatePk, shuffleEncryptWasmFile, shuffleEncryptZkeyFile);
-        assert(await snarkjs.groth16.verify(shuffleEncryptVkey, shuffleEncryptOutput.publicSignals, shuffleEncryptOutput.proof), 'Off-chain verification failed.');
-        let solidityProof: SolidityProof = packToSolidityProof(shuffleEncryptOutput.proof)
-        await shuffleEncryptVerifierContract.verifyProof(
-            [solidityProof[0], solidityProof[1]],
-            [[solidityProof[2], solidityProof[3]], [solidityProof[4], solidityProof[5]]],
-            [solidityProof[6], solidityProof[7]],
-            shuffleEncryptOutput.publicSignals,
-        );
-        // In shuffleEncryptOutput.publicSignals, there are 209 uint256 in total.
-        // 0~207 are card output after shuffle & encrypt, 208~415 are cards before shuffle & encryption.
-        // 416~417 is the aggregatePk.
-        const AliceShuffleEncryptDecks = shuffleEncryptOutput.publicSignals.slice(0, 208);
-        // Checks if shuffle encrypt in plaintext matches circuits.
-        let plaintext_output = shuffleEncryptPlaintext(babyjub, Number(numCards), A, X, R, aggregatedPkEC);
-        for (let i = 0; i < 208; i++) {
-            assert(BigInt(shuffleEncryptOutput.publicSignals[i]) === plaintext_output[i]);
-        }
-        console.log('Alice shuffled the card!')
-
-        // Bob's turn!
-        A = samplePermutation(Number(numCards));
-        R = sampleFieldElements(babyjub, numBits, numCards);
-        shuffleEncryptOutput = await generateShuffleEncryptProof(A, string2Bigint(AliceShuffleEncryptDecks), R, aggregatePk, shuffleEncryptWasmFile, shuffleEncryptZkeyFile);
-        assert(await snarkjs.groth16.verify(shuffleEncryptVkey, shuffleEncryptOutput.publicSignals, shuffleEncryptOutput.proof), 'Off-chain verification failed.');
-        solidityProof = packToSolidityProof(shuffleEncryptOutput.proof)
-        await shuffleEncryptVerifierContract.verifyProof(
-            [solidityProof[0], solidityProof[1]],
-            [[solidityProof[2], solidityProof[3]], [solidityProof[4], solidityProof[5]]],
-            [solidityProof[6], solidityProof[7]],
-            shuffleEncryptOutput.publicSignals,
-        );
-        const BobShuffleEncryptDecks = shuffleEncryptOutput.publicSignals.slice(0, 208);
-        console.log('Bob shuffled the card!')
-
-        // Charlie's turn!
-        A = samplePermutation(Number(numCards));
-        R = sampleFieldElements(babyjub, numBits, numCards);
-        shuffleEncryptOutput = await generateShuffleEncryptProof(A, string2Bigint(BobShuffleEncryptDecks), R, aggregatePk, shuffleEncryptWasmFile, shuffleEncryptZkeyFile);
-        assert(await snarkjs.groth16.verify(shuffleEncryptVkey, shuffleEncryptOutput.publicSignals, shuffleEncryptOutput.proof), 'Off-chain verification failed.');
-        solidityProof = packToSolidityProof(shuffleEncryptOutput.proof)
-        await shuffleEncryptVerifierContract.verifyProof(
-            [solidityProof[0], solidityProof[1]],
-            [[solidityProof[2], solidityProof[3]], [solidityProof[4], solidityProof[5]]],
-            [solidityProof[6], solidityProof[7]],
-            shuffleEncryptOutput.publicSignals,
-        );
-        const CharlieShuffleEncryptDecks = shuffleEncryptOutput.publicSignals.slice(0, 208);
-        console.log('Charlie shuffled the card!')
-
-        /// Decrypts NumCard2Deal cards
-        for (let i = 0; i < NumCard2Deal; i++) {
-            let Y = []
-            for (let j = 0; j < 4; j++) {
-                Y.push(CharlieShuffleEncryptDecks[j * Number(numCards) + i]);
-            }
-            // assign a default proof so the code can compile
-            let decryptProof: FullProof = {
-                proof: {
-                    pi_a: ['0'],
-                    pi_b: [['0']],
-                    pi_c: ['0'],
-                    protocol: '',
-                    curve: '',
-                },
-                publicSignals: ['0']
-            };
-            for (let j = 0; j < 3; j++) {
-                decryptProof = await generateDecryptProof(string2Bigint(Y), skArray[(Number(i) + j) % 3], pkArray[(i + j) % 3], decryptWasmFile, decryptZkeyFile);
-                assert(await snarkjs.groth16.verify(decryptVkey, decryptProof.publicSignals, decryptProof.proof), 'Off-chain verification failed.');
-                let solidityProof: SolidityProof = packToSolidityProof(decryptProof.proof)
-                await decryptVerifierContract.verifyProof(
-                    [solidityProof[0], solidityProof[1]],
-                    [[solidityProof[2], solidityProof[3]], [solidityProof[4], solidityProof[5]]],
-                    [solidityProof[6], solidityProof[7]],
-                    decryptProof.publicSignals,
-                );
-                // publicSignals contain 8 values.
-                // 1~2 is the card value, 3~6 is the Y, 7～8 is the personal public key.
-                Y = [Y[0], Y[1], decryptProof.publicSignals[0], decryptProof.publicSignals[1]];
-            }
-            const cardIdx = searchDeck(X, BigNumber.from(decryptProof.publicSignals[0]).toBigInt(), Number(numCards));
-            console.log('cardIdx: ', cardIdx);
-        }
-        console.log('Decrypt Done!!!')
-    })
 
     it('Shuffle contract v2 can function normally', async () => {
         // Load metadata.
