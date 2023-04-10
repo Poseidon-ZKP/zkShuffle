8973bd4575b190e87967a2681d1ef354780795e2
sam <geeksam820@gmail.com>
2023-04-10 12:39:50 +0800

    cleanup code

--- packages/contracts/contracts/shuffle_encrypt/shuffle_encrypt.sol
+++
@@ -1,5 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17 || ^0.8.4;
-
-// Import this contract so we can compile it
-import "@poseidon-zkp/poseidon-zk-circuits/contracts/shuffle_encrypt_verifier.sol";
