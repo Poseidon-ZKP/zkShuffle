8ed742a3a6d1e516ca8f6f044a11c3623463b920
jacobdenver <jacob@pdn.xyz>
2023-04-21 11:02:32 +0800

    fix: add more events

--- examples/zk-hilo/contracts/contracts/game/HiLo.sol
+++ examples/zk-hilo/contracts/contracts/game/HiLo.sol
@@ -28,11 +28,19 @@ contract HiLo is Ownable {
     // Events
     event GameCreated(uint256 gameId, address playerAddress);
     event GameJoined(uint256 gameId, address playerAddress);
+    event DealCard(uint256 gameId, uint256 cardIdx, address player);
+    event ShowCard(
+        uint256 gameId,
+        uint256 cardIdx,
+        uint256 cardValue,
+        address player
+    );
+    event GameEnd(uint256 gameId, address winner);
 
     constructor(address shuffle_) {
         require(shuffle_ != address(0), "empty address");
         shuffleStateMachine = IShuffle(shuffle_);
-        largestGameId = 0;
+        largestGameId = 100;
     }
 
     // Creates a game.
@@ -123,6 +131,8 @@ contract HiLo is Ownable {
             initDelta,
             gameId
         );
+
+        emit DealCard(gameId, cardIdx, msg.sender);
     }
 
     // player shows `cardIdx`-th card.
@@ -155,6 +165,7 @@ contract HiLo is Ownable {
             cardValue != shuffleStateMachine.INVALID_CARD_INDEX(),
             "Invalid card value"
         );
+        emit ShowCard(gameId, cardIdx, cardValue, msg.sender);
 
         games[gameId].cardValues[playerIdx] = cardValue + 1;
 
@@ -163,6 +174,7 @@ contract HiLo is Ownable {
                 games[gameId].cardValues[1 - playerIdx]
                 ? games[gameId].players[playerIdx]
                 : games[gameId].players[1 - playerIdx];
+            emit GameEnd(gameId, games[gameId].winner);
         }
     }
 
