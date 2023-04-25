fb12649c6ff2b5f10e16b27752746b1f89a89082
jacobdenver <jacob@pdn.xyz>
2023-04-25 16:04:40 +0800

    feat: add ks tests

--- examples/king-soldier/contracts/contracts/game/KS.sol
+++ examples/king-soldier/contracts/contracts/game/KS.sol
@@ -21,7 +21,7 @@ struct Game {
 }
 
 contract KS {
-    uint256 public largestGameID;
+    uint256 public largestgameId;
 
     IShuffle public shuffle1;
     IShuffle public shuffle2;
@@ -32,9 +32,17 @@ contract KS {
     uint256 public constant INVALID_CARD_INDEX = 999999;
 
     // Events
-    event GameCreated(uint256 gameID, address playerAddress);
-    event GameJoined(uint256 gameID, address playerAddress);
-    event GameEnded(uint256 gameID, address winner);
+    event GameCreated(uint256 gameId, address player);
+    event GameJoined(uint256 gameId, address player);
+    event ShuffleDeck(uint256 gameId, address player);
+    event DealCard(uint256 gameId, uint256 cardIdx, address player);
+    event ChooseCard(
+        uint256 gameId,
+        uint256 cardIdx,
+        address player,
+        uint256 round
+    );
+    event GameEnded(uint256 gameId, address winner);
 
     constructor(address shuffle1_, address shuffle2_) {
         require(shuffle1_ != address(0), "shuffle1 empty address");
@@ -43,43 +51,42 @@ contract KS {
         shuffle1 = IShuffle(shuffle1_);
         shuffle2 = IShuffle(shuffle2_);
 
-        largestGameID = 1;
+        largestgameId = 10;
     }
 
     // Creates a game.
     function createGame(uint256[2] memory pk, CharactorType ct) external {
-        uint256 gameID = largestGameID;
+        ++largestgameId;
+        uint256 gameId = largestgameId;
 
-        shuffle1.setGameSettings(2, gameID);
-        shuffle2.setGameSettings(2, gameID);
-        shuffle1.register(msg.sender, pk, gameID);
-        shuffle2.register(msg.sender, pk, gameID);
+        shuffle1.setGameSettings(2, gameId);
+        shuffle2.setGameSettings(2, gameId);
+        shuffle1.register(msg.sender, pk, gameId);
+        shuffle2.register(msg.sender, pk, gameId);
 
-        games[gameID].players[0] = msg.sender;
-        games[gameID].cts[0] = ct;
+        games[gameId].players[0] = msg.sender;
+        games[gameId].cts[0] = ct;
 
-        largestGameID++;
-
-        emit GameCreated(gameID, msg.sender);
+        emit GameCreated(gameId, msg.sender);
     }
 
-    // Joins a game with `gameID`.
+    // Joins a game with `gameId`.
     function joinGame(
-        uint256 gameID,
+        uint256 gameId,
         uint256[2] memory pk,
         CharactorType ct
     ) external {
-        require(games[gameID].players[0] != address(0), "game not created");
-        require(msg.sender != games[gameID].players[0], "same player");
-        require(games[gameID].cts[0] != ct, "same charactor");
+        require(games[gameId].players[0] != address(0), "game not created");
+        require(msg.sender != games[gameId].players[0], "same player");
+        require(games[gameId].cts[0] != ct, "same charactor");
 
-        games[gameID].players[1] = msg.sender;
-        games[gameID].cts[1] = ct;
+        games[gameId].players[1] = msg.sender;
+        games[gameId].cts[1] = ct;
 
-        shuffle1.register(msg.sender, pk, gameID);
-        shuffle2.register(msg.sender, pk, gameID);
+        shuffle1.register(msg.sender, pk, gameId);
+        shuffle2.register(msg.sender, pk, gameId);
 
-        emit GameJoined(gameID, msg.sender);
+        emit GameJoined(gameId, msg.sender);
     }
 
     // Shuffles the deck.
@@ -88,17 +95,19 @@ contract KS {
         uint256[8] calldata proof2,
         uint256[13] calldata shuffle1Data,
         uint256[13] calldata shuffle2Data,
-        uint256 gameID
+        uint256 gameId
     ) external {
-        shuffleOneDeck(proof1, shuffle1Data, shuffle1, gameID);
-        shuffleOneDeck(proof2, shuffle2Data, shuffle2, gameID);
+        shuffleOneDeck(proof1, shuffle1Data, shuffle1, gameId);
+        shuffleOneDeck(proof2, shuffle2Data, shuffle2, gameId);
+
+        emit ShuffleDeck(gameId, msg.sender);
     }
 
     function shuffleOneDeck(
         uint256[8] calldata proof,
         uint256[13] calldata shuffleData,
         IShuffle s,
-        uint256 gameID
+        uint256 gameId
     ) internal {
         uint256 nonce;
         uint256[5] memory shuffledX0;
@@ -123,19 +132,19 @@ contract KS {
             shuffledX0,
             shuffledX1,
             selector,
-            gameID
+            gameId
         );
     }
 
     function getPlayerIndex(
-        uint256 gameID,
+        uint256 gameId,
         address player
     ) public view returns (uint256) {
         require(player != address(0), "empty address");
-        if (games[gameID].players[0] == player) {
+        if (games[gameId].players[0] == player) {
             return 0;
         }
-        if (games[gameID].players[1] == player) {
+        if (games[gameId].players[1] == player) {
             return 1;
         }
         return INVALID_CARD_INDEX;
@@ -143,13 +152,13 @@ contract KS {
 
     // Deal cardIdx-th card to opponent
     function dealHandCard(
-        uint256 gameID,
+        uint256 gameId,
         uint256 cardIdx,
         uint256[8] memory proof,
         uint256[2] memory decryptedCard,
         uint256[2] memory initDelta
     ) external {
-        uint256 playerIdx = getPlayerIndex(gameID, msg.sender);
+        uint256 playerIdx = getPlayerIndex(gameId, msg.sender);
         require(playerIdx != INVALID_CARD_INDEX, "invalid player");
 
         // player1 deal shuffle2 and player2 deal shuffle1
@@ -162,40 +171,44 @@ contract KS {
             proof,
             decryptedCard,
             initDelta,
-            gameID
+            gameId
         );
+
+        emit DealCard(gameId, cardIdx, msg.sender);
     }
 
     function chooseCard(
-        uint256 gameID,
+        uint256 gameId,
         uint256 cardIdx,
         uint256 round
     ) external {
-        require(round == games[gameID].currentRound, "not in current round");
+        require(round == games[gameId].currentRound, "not in current round");
 
-        uint256 playerIdx = getPlayerIndex(gameID, msg.sender);
+        uint256 playerIdx = getPlayerIndex(gameId, msg.sender);
         require(playerIdx != INVALID_CARD_INDEX, "invalid player");
 
-        games[gameID].roundCardChoice[round][playerIdx] = cardIdx;
+        games[gameId].roundCardChoice[round][playerIdx] = cardIdx;
+
+        emit ChooseCard(gameId, cardIdx, msg.sender, round);
     }
 
     // Shows hand card values.
     function showHand(
-        uint256 gameID,
+        uint256 gameId,
         uint256 round,
         uint256[8] memory proof,
         uint256[2] memory decryptedCard
     ) external {
         require(
-            games[gameID].winner == address(0),
+            games[gameId].winner == address(0),
             "this round already has a winner"
         );
-        require(round == games[gameID].currentRound, "not in current round");
+        require(round == games[gameId].currentRound, "not in current round");
 
-        uint256 playerIdx = getPlayerIndex(gameID, msg.sender);
+        uint256 playerIdx = getPlayerIndex(gameId, msg.sender);
         require(playerIdx != INVALID_CARD_INDEX, "invalid player");
 
-        uint256 cardIdx = games[gameID].roundCardChoice[round][playerIdx];
+        uint256 cardIdx = games[gameId].roundCardChoice[round][playerIdx];
 
         // player1 deal shuffle1 and player2 deal shuffle2
         IShuffle s = playerIdx == 0 ? shuffle1 : shuffle2;
@@ -206,30 +219,30 @@ contract KS {
             proof,
             decryptedCard,
             [uint256(0), uint256(0)], // No need for initDelta since this card has been dealt before
-            gameID
+            gameId
         );
 
-        uint256 cardValue = s.search(cardIdx, gameID);
+        uint256 cardValue = s.search(cardIdx, gameId);
         require(cardValue != s.INVALID_CARD_INDEX(), "Invalid card value");
 
         // prevent 0
-        games[gameID].roundCardValue[round][playerIdx] = cardValue + 1;
+        games[gameId].roundCardValue[round][playerIdx] = cardValue + 1;
 
         // both players showed card, start to battle
-        if (games[gameID].roundCardValue[round][1 - playerIdx] != 0) {
-            if (games[gameID].cts[0] == CharactorType.King) {
+        if (games[gameId].roundCardValue[round][1 - playerIdx] != 0) {
+            if (games[gameId].cts[0] == CharactorType.King) {
                 battle(
-                    gameID,
+                    gameId,
                     0,
-                    games[gameID].roundCardValue[round][0],
-                    games[gameID].roundCardValue[round][1]
+                    games[gameId].roundCardValue[round][0],
+                    games[gameId].roundCardValue[round][1]
                 );
             } else {
                 battle(
-                    gameID,
+                    gameId,
                     1,
-                    games[gameID].roundCardValue[round][1],
-                    games[gameID].roundCardValue[round][0]
+                    games[gameId].roundCardValue[round][1],
+                    games[gameId].roundCardValue[round][0]
                 );
             }
         }
@@ -237,52 +250,68 @@ contract KS {
 
     // King > Citizen, Citizen > Soldier, Soldier > King
     function battle(
-        uint256 gameID,
+        uint256 gameId,
         uint256 kingPlayerIdx,
         uint256 kingValue,
         uint256 soldierValue
     ) internal {
         if (kingValue > 1 && soldierValue > 1) {
-            games[gameID].currentRound += 1;
+            games[gameId].currentRound += 1;
             return;
         }
         if (kingValue == 1 && soldierValue == 1) {
             // soldier wins
-            games[gameID].winner = games[gameID].players[1 - kingPlayerIdx];
+            games[gameId].winner = games[gameId].players[1 - kingPlayerIdx];
         } else {
-            games[gameID].winner = games[gameID].players[kingPlayerIdx];
+            games[gameId].winner = games[gameId].players[kingPlayerIdx];
         }
-        emit GameEnded(gameID, games[gameID].winner);
+        emit GameEnded(gameId, games[gameId].winner);
+    }
+
+    function queryAggregatedPk(
+        uint256 gameId,
+        uint256 deckIdx
+    ) external view returns (uint256[2] memory) {
+        IShuffle s = deckIdx == 0 ? shuffle1 : shuffle2;
+        return s.queryAggregatedPk(gameId);
+    }
+
+    function queryDeck(
+        uint256 gameId,
+        uint256 deckIdx
+    ) external view returns (Deck memory) {
+        IShuffle s = deckIdx == 0 ? shuffle1 : shuffle2;
+        return s.queryDeck(gameId);
     }
 
     function queryCardFromDeck(
-        uint256 gameID,
+        uint256 gameId,
         uint256 cardIdx,
         uint256 deckIdx
     ) external view returns (uint256[4] memory) {
         IShuffle s = deckIdx == 0 ? shuffle1 : shuffle2;
-        return s.queryCardFromDeck(cardIdx, gameID);
+        return s.queryCardFromDeck(cardIdx, gameId);
     }
 
     function queryCardInDeal(
-        uint256 gameID,
+        uint256 gameId,
         uint256 cardIdx,
         uint256 deckIdx
     ) external view returns (uint256[4] memory) {
         IShuffle s = deckIdx == 0 ? shuffle1 : shuffle2;
-        return s.queryCardInDeal(cardIdx, gameID);
+        return s.queryCardInDeal(cardIdx, gameId);
     }
 
     function getCardValue(
-        uint256 gameID,
+        uint256 gameId,
         uint256 cardIdx,
         uint256 deckIdx
     ) public view returns (uint256) {
         IShuffle s = deckIdx == 0 ? shuffle1 : shuffle2;
-        return s.search(cardIdx, gameID);
+        return s.search(cardIdx, gameId);
     }
 
-    function getWinner(uint256 gameID) public view returns (address) {
-        return games[gameID].winner;
+    function getGameInfo(uint256 gameId) public view returns (Game memory) {
+        return games[gameId];
     }
 }
