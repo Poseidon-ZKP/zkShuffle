18fc666d96b7cde44793ab3a007dc39626a5229a
sam <geeksam820@gmail.com>
2023-05-09 22:13:21 +0800

    player shuffle

--- packages/contracts/contracts/debug/Debug.sol
+++ packages/contracts/contracts/debug/Debug.sol
@@ -21,6 +21,7 @@ abstract contract Debug is Storage {
             console.log("Invalid numCards ", numCards);
             assert(false);
         }
+
     }
 
     function set_gameState(
@@ -28,6 +29,22 @@ abstract contract Debug is Storage {
         BaseState state
     ) external {
         gameStates[gameId].state = state;
+    }
+
+    function initDeck(
+        uint gameId
+    ) external {
+        ShuffleGameState storage state = gameStates[gameId];
+        zkShuffleCrypto.initDeck(state.deck);
+    }
+
+    function set_playerTurn(
+        uint gameId,
+        address player
+    ) external {
+        ShuffleGameState storage state = gameStates[gameId];
+        state.playerAddrs.push(player);
+        state.playerAddrs[state.curPlayerIndex] = player;
     }
 
 }
--- packages/contracts/tests/unit.ts
+++ packages/contracts/tests/unit.ts
@@ -159,7 +159,6 @@ describe('zkShuffle State Less Unit Test', function () {
         // player 0 Register
         async function playerRegister(pid : number) {
             const player : zkShuffle = players[pid]
-            // check Register Event
             return await ShuffleManager__factory.connect(SM.address, player.owner).playerRegister(
                 gameId,
                 player.owner.address,
@@ -168,11 +167,61 @@ describe('zkShuffle State Less Unit Test', function () {
             )
         }
 
+        // check Register Event
         await expect(playerRegister(0)).to.emit(SM, "Register").withArgs(gameId, 0, players[0].owner.address)
         await expect(playerRegister(1)).to.emit(SM, "Register").withArgs(gameId, 1, players[1].owner.address)
 
         // check Game Full
         await expect(playerRegister(2)).to.be.revertedWith("Game full");
+    });
+
+    it('Player Shuffle', async () => {
+        const SM = await deploy_shuffle_manager(sm_owner)
+        const gameId = 1
+        const numCards = 5
+        const numPlayers = 2
+        let players : zkShuffle[] = []
+        for (let i = 0; i < 9; i++) {
+            players.push(new zkShuffle(SM, signers[i]))
+            await players[i].init()
+        }
+
+        //  prerequisite 1 : Init Game Info
+        await SM.set_gameInfo(gameId, numCards, numPlayers)
+
+        //  prerequisite 2 : Init Game State : Shuffle
+        await SM.set_gameState(gameId, BaseState.Shuffle)
+        expect((await SM.gameState(gameId)).toNumber()).equal(BaseState.Shuffle)
+
+        //  prerequisite 3 : Init Deck
+        await SM.initDeck(gameId)
+
+        //  prerequisite 4 : Set Player's Turn
+        await SM.set_playerTurn(gameId, players[0].owner.address)
+
+        async function playerShuffle(
+            pid : number
+        ) {
+            const player : zkShuffle = players[pid]
+            const numCards = (await SM.gameCardNum(gameId)).toNumber()
+            let shuffleFullProof = await player.generate_shuffle_proof(gameId)
+            let solidityProof: SolidityProof = packToSolidityProof(shuffleFullProof.proof);
+            let compressDeck =  {
+                config : await SM.cardConfig(gameId) ,
+                X0 : shuffleFullProof.publicSignals.slice(3 + numCards * 2, 3 + numCards * 3),
+                X1 : shuffleFullProof.publicSignals.slice(3 + numCards * 3, 3 + numCards * 4),
+                selector0 : { _data : shuffleFullProof.publicSignals[5 + numCards * 4]},
+                selector1 : { _data : shuffleFullProof.publicSignals[6 + numCards * 4]}
+            }
+
+            return await ShuffleManager__factory.connect(SM.address, player.owner).playerShuffle(
+                gameId,
+                solidityProof,
+                compressDeck
+            )
+        }
+
+        await expect(playerShuffle(0)).to.emit(SM, "PlayerTurn").withArgs(gameId, 1, BaseState.Shuffle)
     });
 
 });
