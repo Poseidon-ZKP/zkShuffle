82ac7f58fde1179df58166b28384344362eb9807
Clickcheng110110 <136178955+Clickcheng110110@users.noreply.github.com>
2023-06-12 19:30:34 +0800

    fix:package tsconfigs and publish bugs (#79)
    * feat:add lint prettier vscode config
    
    * fix:add pre-commit
    
    * feat:close pre-commit
    
    * merge:main
    
    * fefa:update yarn.lock
    
    * fix:try to fix CI bug
    
    * fix:try to fix ci bug
    
    * fix CI
    
    * fix:add src fodler in jssdk
    
    * fix:jssdk tsconfig.json
    
    * feat:add jssdk compile in ci
    
    * fix:remove index.ts in jssdk
    
    * fix:update import path
    
    * feat:update
    
    * feat:update package.json dependence
    
    * feat:update packages version
    
    * fix:packages.json
    
    * fix:update proof compile  outdir
    
    * feat:update proof outdir
    
    * fix:publish error
    
    * feat:update version 1.4.0
    
    * feat:update jssdk esm outdir name
    
    * feat:update publish version
    
    * fix:axios version
    
    * yarn.lock from sam
    
    * feat:update jssdk ts config
    
    * fix:update jssdk tsconfig
    
    * fix yarn.lock
    
    * fix:ci
    
    * feat:update jssdk ts config
    
    * feat:update yarn.lock
    
    * update publish version
    
    * feat:update version
    
    * feat:update
    
    * fix:update jssdk version
    
    * feat:build contracts module dist
    
    * feat:update contracts dist files
    
    * feat:update yarn.lock
    
    * feat:update conracts module npm publish config
    
    * fix:update npm publish ci
    
    * feat:update contracts package.json files
    
    * feat:updadte contracts files config
    
    * feat:update
    
    * feat:update contracts
    
    * feat:update contracts
    
    * feat:update
    
    * feat:update jssdk gitingore
    
    * fix:remove compile files
    
    * fix:update yarn.lock
    
    * fix:remove proof dist files
    
    * fix:restore .version
    
    ---------
    
    Co-authored-by: 亮程 <cl510804194@gmail.com>
    Co-authored-by: sam <geeksam820@gmail.com>

--- packages/circuits/hardhat.config.ts
+++ packages/circuits/hardhat.config.ts
@@ -1,15 +1,15 @@
-import { HardhatUserConfig } from 'hardhat/config';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import { config as dotenvConfig } from "dotenv";
-import { resolve } from 'path';
+import { resolve } from "path";
 
 // setup the environment variables
 dotenvConfig({ path: resolve(__dirname, "./.env") });
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.4',
+  solidity: "0.8.4",
   typechain: {
     outDir: "types",
     target: "ethers-v5",
@@ -18,7 +18,7 @@ const config: HardhatUserConfig = {
     tests: "./tests",
   },
   mocha: {
-    timeout: 100000000
+    timeout: 100000000,
   },
 };
 
--- packages/contracts/helper/deploy.ts
+++ packages/contracts/helper/deploy.ts
@@ -1,65 +1,62 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { ethers } from "hardhat";
-import { DecryptVerifier__factory, Shuffle_encryptVerifier__factory, ShuffleManager__factory, Shuffle_encryptVerifier5Card__factory, Shuffle_encryptVerifier30Card__factory, Test__factory } from "../types";
-
+import {
+  DecryptVerifier__factory,
+  Shuffle_encryptVerifier__factory,
+  ShuffleManager__factory,
+  Shuffle_encryptVerifier5Card__factory,
+  Shuffle_encryptVerifier30Card__factory,
+  Test__factory,
+} from "../types";
 
 // Depploys contract for decryption.
-async function deployDecrypt(owner : SignerWithAddress) {
-    return await (new DecryptVerifier__factory(owner)).deploy()
+async function deployDecrypt(owner: SignerWithAddress) {
+  return await new DecryptVerifier__factory(owner).deploy();
 }
 
 // Deploys contract for shuffle encrypt.
-async function deployShuffleEncrypt(owner : SignerWithAddress) {
-    return await (new Shuffle_encryptVerifier__factory(owner)).deploy()
+async function deployShuffleEncrypt(owner: SignerWithAddress) {
+  return await new Shuffle_encryptVerifier__factory(owner).deploy();
 }
 
-async function deployShuffleEncryptCARD30(owner : SignerWithAddress) {
-    return await (new Shuffle_encryptVerifier30Card__factory(owner)).deploy()
+async function deployShuffleEncryptCARD30(owner: SignerWithAddress) {
+  return await new Shuffle_encryptVerifier30Card__factory(owner).deploy();
 }
 
-
-async function deployShuffleEncryptCARD5(owner : SignerWithAddress) {
-    return await (new Shuffle_encryptVerifier5Card__factory(owner)).deploy()
+async function deployShuffleEncryptCARD5(owner: SignerWithAddress) {
+  return await new Shuffle_encryptVerifier5Card__factory(owner).deploy();
 }
 
-export async function deploy_shuffle_manager(owner : SignerWithAddress) {
+export async function deploy_shuffle_manager(owner: SignerWithAddress) {
+  const encrypt52 = await deployShuffleEncrypt(owner);
+  const encrypt30 = await deployShuffleEncryptCARD30(owner);
+  const encrypt5 = await deployShuffleEncryptCARD5(owner);
+  const decrypt = await deployDecrypt(owner);
 
-    const encrypt52 = await deployShuffleEncrypt(owner);
-    const encrypt30 = await deployShuffleEncryptCARD30(owner);
-    const encrypt5  = await deployShuffleEncryptCARD5(owner)
-    const decrypt = await deployDecrypt(owner);
-
-    const crypto = await (await ethers.getContractFactory('zkShuffleCrypto')).deploy();
-    const sm = await (await ethers.getContractFactory('ShuffleManager', {
-        libraries: {
-            zkShuffleCrypto: crypto.address,
-        }
-    })).deploy(
-        decrypt.address,
-        encrypt52.address,
-        encrypt30.address,
-        encrypt5.address
-    );
-    return ShuffleManager__factory.connect(sm.address, owner)
+  const crypto = await (await ethers.getContractFactory("zkShuffleCrypto")).deploy();
+  const sm = await (
+    await ethers.getContractFactory("ShuffleManager", {
+      libraries: {
+        zkShuffleCrypto: crypto.address,
+      },
+    })
+  ).deploy(decrypt.address, encrypt52.address, encrypt30.address, encrypt5.address);
+  return ShuffleManager__factory.connect(sm.address, owner);
 }
 
-export async function deploy_shuffle_test(owner : SignerWithAddress) {
+export async function deploy_shuffle_test(owner: SignerWithAddress) {
+  const encrypt52 = await deployShuffleEncrypt(owner);
+  const encrypt30 = await deployShuffleEncryptCARD30(owner);
+  const encrypt5 = await deployShuffleEncryptCARD5(owner);
+  const decrypt = await deployDecrypt(owner);
 
-    const encrypt52 = await deployShuffleEncrypt(owner);
-    const encrypt30 = await deployShuffleEncryptCARD30(owner);
-    const encrypt5  = await deployShuffleEncryptCARD5(owner)
-    const decrypt = await deployDecrypt(owner);
-
-    const crypto = await (await ethers.getContractFactory('zkShuffleCrypto')).deploy();
-    const sm = await (await ethers.getContractFactory('Test', {
-        libraries: {
-            zkShuffleCrypto: crypto.address,
-        }
-    })).deploy(
-        decrypt.address,
-        encrypt52.address,
-        encrypt30.address,
-        encrypt5.address
-    );
-    return Test__factory.connect(sm.address, owner)
+  const crypto = await (await ethers.getContractFactory("zkShuffleCrypto")).deploy();
+  const sm = await (
+    await ethers.getContractFactory("Test", {
+      libraries: {
+        zkShuffleCrypto: crypto.address,
+      },
+    })
+  ).deploy(decrypt.address, encrypt52.address, encrypt30.address, encrypt5.address);
+  return Test__factory.connect(sm.address, owner);
 }
